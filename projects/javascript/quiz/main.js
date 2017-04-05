e3b23d5ecd20cf6aaec7e7f970f131dec189b531
jom663 <jom663@newschool.edu>
2017-04-05 19:45:45 -0400

    js quiz

---
+++ projects/javascript/quiz/main.js
@@ -0,0 +1,35 @@
+var myName = 'Moonnyung Jo';
+var favoriteNumber = 7;
+var favoriteNumberst = '7';
+var bValue = [true];
+var firstName = ['M', 'O', 'O', 'N', 'N', 'Y', 'U', 'N', 'G'];
+var myArray = ['bubbletea', true, 1120];
+
+console.log(myName);
+console.log(favoriteNumber);
+console.log(favoriteNumberst);
+console.log(bValue);
+console.log(firstName);
+console.log(myArray);
+console.log('hot' + 'dog');
+console.log(49 + 51);
+
+var firstItem = firstName[0];
+var lastItem = firstName[firstName.length - 1];
+
+var myArray2 = ['s', 'u', 'p', 'e', 'r', 'c', 'a', 'l', 'i', 'f', 'r', 'a', 'g', 'i', 'l', 'i', 's', 't', 'i', 'c', 'e', 'x', 'p', 'i', 'a', 'l', 'i', 'd', 'o', 'c', 'i', 'o', 'u',  's'];
+console.log(myArray2.length);
+
+var allStudentsHere = true;
+
+if (allStudentsHere = true){console.log('all the students are in class');} else {console.log('not everyone is here');}
+  
+for(console.log(firstName));
+
+var time = date.getTime();
+
+if ( time > 7pm && time < 9:40pm ) {
+  console.log(true);
+} else {
+  console.log(false);
+}
