6ede1c038f1aca563b89b97e7c4739c3eb1e957b
jom663 <jom663@newschool.edu>
2017-04-26 19:21:23 -0400

    exercise

--- projects/exercise/7. Random Boxes/main.js
+++ projects/exercise/7. Random Boxes/main.js
@@ -1,3 +1,33 @@
-$(function() {
-  // Your interactions go here
+//$(document).ready(function(){
+//    $(".box").click(function(){
+//      var items = $(".boxes");
+//      var item = Math.floor(Math.random() * items.length);
+//      
+//      if($(items[item]).css('background-color') == "rgb(255, 255, 255)") {
+//        $(this).css({backgroundColor:"#6A37B7", color:"rgb(255, 255, 255)"});
+//    }
+//     else {
+//        $(this).css({backgroundColor:"#rgb(255, 255, 255)", color:"#6A37B7"});
+//     }
+//    });
+//});
+
+$(document).ready(function(){
+    $(".box").click(function(){
+       var items = $(".boxes");
+var item = Math.floor(Math.random() * items.length);
+      $(items[item]).css({backgroundColor:"#6A37B7", color:"white"});
+    });
 });
+
+//$(document).ready(function(){
+//    $(".box").click(function(){
+//       var items = $(".boxes");
+//var item = Math.floor(Math.random() * items.length);
+//      if($(".boxes").hasClass('active')) {
+//      element.removeClass.('active');}
+//      else
+//      {$(items[item]).addClass("active");}
+//      
+//    });
+//});
--- projects/exercise/8. Extra Credit/main.js
+++ projects/exercise/8. Extra Credit/main.js
@@ -1,3 +1,24 @@
-$(function() {
-  // Your interactions go here
-});
+
+   $(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 100) {
+                    $("body").css('background-color', '#99ddff');
+                } else if(scroll_pos > 200 && 100 < scroll_pos)  {console.log ("im a");
+                    $("body").css('background-color', '#33bbff');
+                }
+              else if(scroll_pos > 300 && 200 < scroll_pos)  {console.log ("im b");
+                    $("body").css('background-color', '#0088cc');
+                }
+              else if(scroll_pos > 400 && 300 < scroll_pos)  {
+                    $("body").css('background-color', '#004466');
+                }
+              else if(scroll_pos > 500 && 400 < scroll_pos)  {
+                    $("body").css('background-color', '#000000');
+                }
+              else {$("body").css('background-color', 'white');
+                
+              }
+            });
+        });
