006a91b10ee024638577a78b6cbb076d52c68716
jom663 <jom663@newschool.edu>
2017-04-26 19:59:23 -0400

    first proto

--- projects/exercise/7. Random Boxes/main.js
+++ projects/exercise/7. Random Boxes/main.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
     $(".box").click(function(){
        var items = $(".boxes");
 var item = Math.floor(Math.random() * items.length);
+      $(item).css.({background:''})
       $(items[item]).css({backgroundColor:"#6A37B7", color:"white"});
     });
 });
