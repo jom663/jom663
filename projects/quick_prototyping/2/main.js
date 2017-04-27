1325fc0b8921536b57d560a3df3ff8ea85c61db9
jom663 <jom663@newschool.edu>
2017-04-26 20:41:44 -0400

    change color

--- projects/quick_prototyping/2/main.js
+++ projects/quick_prototyping/2/main.js
@@ -1,3 +1,19 @@
-$(function() {
-  // Your interactions go here
-});
+function time(){
+	var today = new Date();
+	var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+  var r = parseInt(s) * 4;
+  var g = parseInt(s) * 4;
+  var b = parseInt(s) * 4;
+  document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+  var t = setTimeout(time, 500);
+}
+
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};
+	return i;
+}
+
+time();
